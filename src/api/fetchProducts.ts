b95b0f1748dd588612dc83b06ac41c929f086289
Vinicius <vfpv@hotmail.com>
2023-10-23 21:59:07 -0300

    configurações iniciais

---
+++ src/api/fetchProducts.ts
@@ -0,0 +1,12 @@
+type Props = {
+    query: string;
+}
+
+const fetchProducts = async ({ query }: Props) => {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await response.json();
+
+  return data.results;
+};
+
+export default fetchProducts;
---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-yellow-400 w-full h-16">
+      Este é o header
+    </header>
+  );
+};
+
+export default Header;
