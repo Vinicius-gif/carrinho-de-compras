bc9785408f3755eac3b78271f7d4c8846e2636f5
Vinicius-gif <vfpv@hotmail.com>
2023-10-26 12:56:03 -0300

    style: ProductCard component

--- src/app/components/Products/ProductCard/index.tsx
+++ src/app/components/Products/ProductCard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import formatCurrency from '@/utils/formatCurrency';
 import React from 'react';
 import { BsFillCartPlusFill } from 'react-icons/bs';
@@ -10,22 +11,25 @@ export interface CardProps {
 
 const ProductCard = ({title, price, thumbnail}: CardProps) => {
   return (
-    <section className="w-full max-w-xs bg-white flex flex-col cursor-pointer my-0 mx-auto relative">
+    <section className="w-full max-w-xs bg-white flex flex-col cursor-pointer my-0 mx-auto relative hover:shadow-lg hover:flex">
       
       <img
         src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
         alt="product"
-        className="card__image"
+        className="w-full"
       />
 
-      <div className="card__infos">
-        <h2 className="card__price">{formatCurrency({value: price, currency: 'BRL'})}</h2>
-        <h2 className="card__title">{title}</h2>
+      <div className="p-5 border-t border-gray-400">
+        <h2 className="text-3xl font-normal block mb-3">
+          {formatCurrency({value: price, currency: 'BRL'})}
+        </h2>
+        <h2 className="text-sm text-gray-900 leading-6 font-medium">{title}</h2>
       </div>
 
       <button
         type="button"
-        className="button__add-cart"
+        className="absolute top-0 right-0 w-11 h-11 my-3 mx-4 text-blue-600 bg-white items-center
+        justify-center border-none rounded-l-3xl text-2xl cursor-pointer flex"
       >
         <BsFillCartPlusFill />
       </button>
