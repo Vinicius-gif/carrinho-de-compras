7112142a5f01476ca010e0fce8e509033cab3a81
Vinicius-gif <vfpv@hotmail.com>
2023-10-30 15:21:53 -0300

    format: header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,25 +1,14 @@
 'use client';
 
-import React, { useContext } from 'react';
+import React from 'react';
 import SearchBar from './SearchBar';
 import CartButton from './CartButton';
 import Image from 'next/image';
-import { ProductsContext } from '@/app/context/CarrinhoContext';
 
 const Header = () => {
 
-  const context = useContext(ProductsContext);
-
-  if (context === undefined) {
-    return null; 
-  }
-
-  const { products } = context;
-  console.log(products);
-  
-
   return (
-    <header className="bg-yellow-400 w-full h-16 flex items-center">
+    <header className="bg-yellow-400 w-full h-16 flex items-center fixed z-10">
       <Image width={128} height={40} src="/images/logo.png" alt="logo" className="ml-4"/>
       <div className="flex items-center justify-around w-4/5">
         <SearchBar/>
