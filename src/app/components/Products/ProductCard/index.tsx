8c6aee504bfab842499c13770a15cf339c5080d6
Vinicius <vfpv@hotmail.com>
2023-10-31 20:05:50 -0300

    feat: loading

--- src/app/components/Products/ProductCard/index.tsx
+++ src/app/components/Products/ProductCard/index.tsx
@@ -3,8 +3,6 @@ import { ProductsContext } from '@/app/context/ProductsContext';
 import formatCurrency from '@/utils/formatCurrency';
 import React, { useContext} from 'react';
 import { BsFillCartPlusFill } from 'react-icons/bs';
-import Loading from '../loading';
-
 export interface CardProps {
   id: string;
   title: string;
@@ -14,7 +12,7 @@ export interface CardProps {
 
 const ProductCard = ({id, title, price, thumbnail}: CardProps) => {
 
-  const { loading, cartItems, setCartItems } = useContext(ProductsContext);
+  const { cartItems, setCartItems } = useContext(ProductsContext);
 
   const addCartItem = () => {
     setCartItems([...cartItems, {
@@ -26,37 +24,31 @@ const ProductCard = ({id, title, price, thumbnail}: CardProps) => {
   };
 
   return (
-    <>
-      { loading ?
-        <Loading/>
-        :
-        <section 
-          className="group w-full max-w-xs bg-white flex flex-col cursor-pointer my-0 mx-auto relative
+    <section 
+      className="group w-full max-w-xs bg-white flex flex-col cursor-pointer my-0 mx-auto relative
       hover:shadow-lg hover:flex">
-          <img
-            src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
-            alt="product"
-            className="w-full"
-          />
+      <img
+        src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
+        alt="product"
+        className="w-full"
+      />
 
-          <div className="p-5 border-t border-gray-400">
-            <h2 className="text-3xl font-normal block mb-3">
-              {formatCurrency({value: price, currency: 'BRL'})}
-            </h2>
-            <h2 className="text-sm text-gray-900 leading-6 font-medium">{title}</h2>
-          </div>
+      <div className="p-5 border-t border-gray-400">
+        <h2 className="text-3xl font-normal block mb-3">
+          {formatCurrency({value: price, currency: 'BRL'})}
+        </h2>
+        <h2 className="text-sm text-gray-900 leading-6 font-medium">{title}</h2>
+      </div>
 
-          <button
-            type="button"
-            onClick={addCartItem}
-            className="absolute top-0 right-0 w-11 h-11 my-3 mx-4 text-blue-600 bg-white items-center
+      <button
+        type="button"
+        onClick={addCartItem}
+        className="absolute top-0 right-0 w-11 h-11 my-3 mx-4 text-blue-600 bg-white items-center
         justify-center border-none rounded-l-3xl text-2xl cursor-pointer hidden group-hover:flex"
-          >
-            <BsFillCartPlusFill />
-          </button>
-        </section>
-      }
-    </>
+      >
+        <BsFillCartPlusFill />
+      </button>
+    </section>
   );
 };
 
--- src/app/components/Products/index.tsx
+++ src/app/components/Products/index.tsx
@@ -4,10 +4,11 @@ import React, { useEffect, useContext } from 'react';
 import ProductCard from './ProductCard';
 import fetchProducts from '@/api/fetchProducts';
 import { ProductsContext } from '@/app/context/ProductsContext';
+import Loading from './loading';
 
 const Products = () => {
 
-  const {products, setProducts, setLoading} = useContext(ProductsContext);
+  const {products, setProducts, loading, setLoading} = useContext(ProductsContext);
   
   useEffect(() => {
     fetchProducts({query: 'iphone'}).then((response) => {
@@ -17,11 +18,16 @@ const Products = () => {
   }, [setProducts, setLoading]);
 
   return (
-    <section className="items-center justify-center pt-28 mx-28 mb-14 grid gap-6 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-      {products.map((product) => (
-        <ProductCard key={product.id} id={product.id} title={product.title} price={product.price} thumbnail={product.thumbnail}/>
-      ))}
-    </section>
+    <>
+      {loading ? <Loading/>
+        :
+        <section className="items-center justify-center pt-28 mx-28 mb-14 grid gap-6 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+          {products.map((product) => (
+            <ProductCard key={product.id} id={product.id} title={product.title} price={product.price} thumbnail={product.thumbnail}/>
+          ))}
+        </section>
+      }
+    </>
   );
 };
 
