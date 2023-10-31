b6bdceee115259cfdda7d80c986400f4a6bcadf7
Vinicius-gif <vfpv@hotmail.com>
2023-10-31 12:25:26 -0300

    add: limpar carrinho function

--- src/app/components/Carrinho/index.tsx
+++ src/app/components/Carrinho/index.tsx
@@ -3,16 +3,24 @@
 import React, {useContext} from 'react';
 import CartItem from './CartItem';
 import { ProductsContext } from '@/app/context/ProductsContext';
+import formatCurrency from '@/utils/formatCurrency';
+import {MdRemoveShoppingCart} from 'react-icons/md';
+
 
 const Carrinho = () => {
 
-  const { cartItems } = useContext(ProductsContext);
+  const { cartItems, setCartItems, isCartVisible } = useContext(ProductsContext);
 
   const totalPrice = cartItems.reduce((acc, item) => item.price + acc, 0);
+  const priceCurrency = formatCurrency({value: totalPrice, currency: 'BRL'});
+
+  const limparCarrinho = () => {
+    setCartItems([]);
+  };
 
   return (
-    <section className="w-full max-w-xs bg-white h-screen fixed top-0 right-0 px-5 pt-24 pb-5 flex flex-col
-    justify-between transition-all duration-300 ease-linear">
+    <section className={`w-full max-w-xs bg-white h-screen fixed top-0 right-0 px-5 pt-24 pb-5 flex flex-col
+    justify-between transition-all duration-200 ease-linear transform translate-x-full ${isCartVisible ? 'translate-x-0' : ''}`}>
       <div className="grow overflow-auto border-b-0 last:border-b-0">
         { cartItems.map((cartItem) => 
           <CartItem
@@ -24,7 +32,12 @@ const Carrinho = () => {
           />
         )}
       </div>
-      <div className="text-3xl font-medium pt-9 pb-4 border-t border-gray-400 ">Preço toral: {totalPrice}</div>
+      <div className="text-3xl font-medium pt-9 pb-4 border-t border-gray-400 ">Total: {priceCurrency}</div>
+      <div>
+        <button type="button" className="flex items-center font-medium" onClick={limparCarrinho}>
+          Limpar carrinho: <MdRemoveShoppingCart className="w-8 h-8 text-red-700"/>
+        </button>
+      </div>
     </section>
   );
 };
